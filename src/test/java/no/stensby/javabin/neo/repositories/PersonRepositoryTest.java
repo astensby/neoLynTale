ac39456f0c382c2cfc8d802f3a3e0926216fdf1d
Aleksander Stensby <aleksander.stensby@gmail.com>
2014-09-14 22:33:38 +0200

    cleaned up the test case a bit

--- src/test/java/no/stensby/javabin/neo/repositories/PersonRepositoryTest.java
+++ src/test/java/no/stensby/javabin/neo/repositories/PersonRepositoryTest.java
@@ -1,10 +1,7 @@
 package no.stensby.javabin.neo.repositories;
 
-import static org.junit.Assert.assertEquals;
 import no.stensby.javabin.neo.configuration.TestApplicationConfig;
 import no.stensby.javabin.neo.domain.Person;
-
-import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,31 +9,34 @@ import org.springframework.data.neo4j.support.Neo4jTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
 //@TestExecutionListeners({ DependencyInjectionTestExecutionListener.class })
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { TestApplicationConfig.class })
+@ContextConfiguration(classes = {TestApplicationConfig.class})
 public class PersonRepositoryTest {
 
-	@Autowired
-	PersonRepository repository;
+    @Autowired
+    PersonRepository repository;
 
-	@Autowired
-	protected Neo4jTemplate template;
+    @Autowired
+    protected Neo4jTemplate template;
 
-	@Test
-	public void savesPersonCorrectly() {
-		Person p = new Person("Aleksander", "Stensby");
-		Person result = repository.save(p);
+    @Test
+    public void savesPersonCorrectly() {
+        Person p = new Person("Aleksander", "Stensby");
+        Person result = repository.save(p);
 
-		Assert.assertNotNull(result.getNodeId());
+        assertNotNull("persisted person has been assigned a node ID", result.getNodeId());
+        assertEquals("person name matches persisted person name",
+                p.getFirstName(), result.getFirstName());
 
-		Person retrievedP = repository.findOne(result.getNodeId());
+        Person retrievedP = repository.findOne(result.getNodeId());
 
-		assertEquals("retrieved person name matches persisted person name",
-				"Aleksander", retrievedP.getFirstName());
-		assertEquals("retrieved person name matches persisted person name",
-				result.getFirstName(), retrievedP.getFirstName());
+        assertEquals("retrieved person name matches persisted person name",
+                result.getFirstName(), retrievedP.getFirstName());
 
-	}
+    }
 
 }
